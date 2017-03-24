4d44464f23bfa75201849bdf68364e05f37715de
Kaushal Patel <kaushalpatel922@gmail.com>
2017-03-24 15:02:03 -0400

    plotOptions

--- main.js
+++ main.js
@@ -19,7 +19,31 @@ $(function() {
         yAxis: {
             title: {
                 text: 'Revenue in millions'
+            },
+            labels: {
+                formatter: function () {
+                    return '$' + this.value + 'm';
+                }
+            },
+            gridLineWidth: 0.6,
+            maxPadding: 0.1
+        }, 
+        legend: {
+            enabled: false
+        },
+        plotOptions: {
+            series: {
+                borderWidth: 0,
+                dataLabels: {
+                    enabled: true,
+                    format: '${point.y:.1f} m'
+                },
             }
+        },
+        tooltip: {
+            headerFormat: '<span style="font-size:13px">{series.name}</span><br>',
+            pointFormat: '<span style="font-size: 13px""color:{point.color}">{point.name}</span>: <b>${point.y:.1f}m</b><br/>'
+        },
         }
     })
 })
