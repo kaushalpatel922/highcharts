74ead7b47289a6344f36e6e1f6b348d04c2787f2
Kaushal Patel <kaushalpatel922@gmail.com>
2017-03-24 13:00:35 -0400

    defining titles and axes

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
 console.log("Hello World!");
+
+$(function() {
+    //Create the chart
+    Highcharts.chart('container', {
+        chart: {
+            //define the chart type here.
+            type:''
+        },
+        title: {
+            text: 'Chart Analysis'
+        },
+        subtitle: {
+            text: 'Product sales over years'
+        },
+        xAxis: {
+            type: 'Category'
+        },
+        yAxis: {
+            title: {
+                text: 'Revenue in millions'
+            }
+        }
+    })
+})
